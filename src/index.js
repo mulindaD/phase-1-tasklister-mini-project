16837a37967c16d631d6e04413d9bb08b1b07c08
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-07-17 22:58:11 +0300

    Completes first deliverable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const submit = document.querySelector('input[type=submit]')
+
+// Adding a new task below the button and removing it 
+function handleNewTask(task) {
+  const li = document.createElement('li')
+  const removeTask = document.createElement('button')
+  removeTask.textContent = 'x'
+  li.textContent = task + ' '
+  li.appendChild(removeTask)
+  document.querySelector('#tasks').appendChild(li)
+
+  // Removing a task that has been added 
+  removeTask.addEventListener('click', e => {
+    e.target.parentNode.remove()
+  })
+}
+
+// Adding a new task after click
+submit.addEventListener('click', function(e){
+  e.preventDefault()
+  const inputValue = document.querySelector('#new-task-description')
+
+  // Handling a new task 
+  handleNewTask(inputValue.value)
+})
